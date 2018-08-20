a2dd95b7a2cc32865541775b0862ea5178d67868
Dmitry Kandalov <dmitry.kandalov@springernature.com>
2018-08-20 23:39:16 +0100

    live plugin actions to open KString and Array.cpp

--- plugins/kotlin-snake-snippets/plugin.groovy
+++ plugins/kotlin-snake-snippets/plugin.groovy
@@ -1,24 +1,45 @@
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Separator
+import com.intellij.openapi.editor.LogicalPosition
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import liveplugin.PluginUtil
 
 import static liveplugin.PluginUtil.*
 
+def knBasePath = System.getProperty("user.home") + "/IdeaProjects/kotlin-native/"
+
 registerAction("kotlinSnakeProjectPopup", "ctrl shift K") { AnActionEvent event ->
-	def project = event.project
-	def popupMenuDescription = [
-			"gradle":{ pasteGradleRepos(project) },
-			"log":{ pasteLog(project) },
-			"shouldEqual":{ pasteShouldEqual(project) }
-	]
-	showPopupMenu(popupMenuDescription, "Snake")
+    def project = event.project
+    def popupMenuDescription = [
+            "gradle"             : { pasteGradleRepos(project) },
+            "log"                : { pasteLog(project) },
+            "shouldEqual"        : { pasteShouldEqual(project) },
+            "--"                 : Separator.instance,
+            "KString"            : { openInEditor("$knBasePath/runtime/src/main/cpp/KString.cpp", project) },
+            "backend/Collections": {
+                def virtualFile = openInEditor("$knBasePath/backend.native/build/stdlib/kotlin/collections/Collections.kt", project)
+                if (virtualFile != null) currentEditorIn(project).caretModel.moveToLogicalPosition(new LogicalPosition(75, 0))
+            },
+            "runtime/Collections": {
+                def virtualFile = openInEditor("$knBasePath/runtime/src/main/kotlin/kotlin/collections/Collections.kt", project)
+                if (virtualFile != null) currentEditorIn(project).caretModel.moveToLogicalPosition(new LogicalPosition(53, 0))
+            },
+            "Array.kt"           : { openInEditor("$knBasePath/runtime/src/main/kotlin/kotlin/Array.kt", project) },
+            "Array.cpp"          : {
+                def virtualFile = openInEditor("$knBasePath/runtime/src/main/cpp/Arrays.cpp", project)
+                if (virtualFile != null) currentEditorIn(project).caretModel.moveToLogicalPosition(new LogicalPosition(59, 0))
+            },
+    ]
+    showPopupMenu(popupMenuDescription, "Snake")
 }
 
 static pasteGradleRepos(Project project) {
-	def document = currentDocumentIn(project)
-	def editor = currentEditorIn(project)
+    def document = currentDocumentIn(project)
+    def editor = currentEditorIn(project)
 
-	runDocumentWriteAction(project, document) {
-		document.insertString(editor.caretModel.offset, """
+    runDocumentWriteAction(project, document) {
+        document.insertString(editor.caretModel.offset, """
 buildscript {
 	repositories {
 		mavenCentral()
@@ -43,26 +64,26 @@ konanArtifacts {
     }
 }
 """)
-	}
+    }
 }
 
 static pasteShouldEqual(Project project) {
-	def document = currentDocumentIn(project)
-	def editor = currentEditorIn(project)
+    def document = currentDocumentIn(project)
+    def editor = currentEditorIn(project)
 
-	runDocumentWriteAction(project, document) {
-		document.insertString(editor.caretModel.offset, """
+    runDocumentWriteAction(project, document) {
+        document.insertString(editor.caretModel.offset, """
 infix fun <T> T.shouldEqual(that: T) = assertEquals(actual = this, expected = that)
 """)
-	}
+    }
 }
 
 static pasteLog(Project project) {
-	def document = currentDocumentIn(project)
-	def editor = currentEditorIn(project)
+    def document = currentDocumentIn(project)
+    def editor = currentEditorIn(project)
 
-	runDocumentWriteAction(project, document) {
-		document.insertString(editor.caretModel.offset, """fun log(message: String) {
+    runDocumentWriteAction(project, document) {
+        document.insertString(editor.caretModel.offset, """fun log(message: String) {
     val file = fopen("log.txt", "a") ?: error("couldn't open file")
     try {
         val line = message + "\\n"
@@ -72,7 +93,7 @@ static pasteLog(Project project) {
     }
 }
 """)
-	}
+    }
 }
 
 if (!isIdeStartup) show("Reloaded Kotlin snake popup")
